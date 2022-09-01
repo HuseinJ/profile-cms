dbe22c217cd2b0e45ec5d5264a20d4d477f2fd49
Husein Jusic <husein.jusic@icloud.com>
2022-09-01 20:56:31 +0200

    init

---
+++ profile-api/src/main/kotlin/com/hjusic/api/profileapi/ProfileApiApplication.kt
@@ -0,0 +1,11 @@
+package com.hjusic.api.profileapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProfileApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProfileApiApplication>(*args)
+}
---
+++ profile-api/src/test/kotlin/com/hjusic/api/profileapi/ProfileApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hjusic.api.profileapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProfileApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
