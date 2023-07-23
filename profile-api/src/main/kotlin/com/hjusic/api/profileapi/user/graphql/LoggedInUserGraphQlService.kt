710256f5175528ee9c33a8022f12073550008b0b
Husein Jusic <husein.jusic@icloud.com>
2023-07-23 03:08:35 +0200

    feat: add user ui

--- profile-api/src/main/kotlin/com/hjusic/api/profileapi/user/graphql/LoggedInUserGraphQlService.kt
+++ profile-api/src/main/kotlin/com/hjusic/api/profileapi/user/graphql/LoggedInUserGraphQlService.kt
@@ -3,12 +3,14 @@ package com.hjusic.api.profileapi.user.graphql
 import com.hjusic.api.profileapi.common.security.UserAuthServices
 import com.netflix.graphql.dgs.DgsComponent
 import com.netflix.graphql.dgs.DgsQuery
+import org.springframework.security.access.annotation.Secured
 
 @DgsComponent
 class LoggedInUserGraphQlService(
     val userAuthServices: UserAuthServices
 ) {
 
+    @Secured
     @DgsQuery
     fun loggedInUser(): UserGraphQlView? {
         var loggedInUser = userAuthServices?.callingUser()
