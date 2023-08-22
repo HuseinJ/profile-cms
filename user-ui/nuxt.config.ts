f9b041c59aa42aa78001d3cdf1e1f391baf854dd
Husein Jusic <husein.jusic@icloud.com>
2023-08-22 22:16:23 +0200

    fix: fix static file generation. Use axios to get page names

--- user-ui/nuxt.config.ts
+++ user-ui/nuxt.config.ts
@@ -1,19 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { gql } from '@apollo/client/core';
-
+import axios from 'axios'
 async function getRoutes(apollo:any) {
-  const { data } = await apollo.query({
-    query: gql`
-      query {
-        pages{
-          name
-        }
-      }
-    `
-  });
-  return data.pages.map((page) => (`/${page.name}`));
+  return axios({
+    url: 'http://api.testing.hjusic.com:8080' + '/graphql',
+    method: 'post',
+    data: {
+      query: `
+                query {
+                  pages{
+                    name
+                  }
+                }
+            `,
+    },
+  }).then((result) => {
+    return result.data.data.pages.map((page) => {
+      return '/' + page.name
+    })
+  })
 }
-
 export default defineNuxtConfig({
   modules: ['@nuxtjs/apollo'],
   apollo: {
@@ -26,7 +31,6 @@ export default defineNuxtConfig({
   hooks: {
     async 'nitro:config'(nitroConfig) {
       if (nitroConfig.dev) return
-
       let slugs = await getRoutes();
       nitroConfig.prerender.routes.push(...slugs);
       return
