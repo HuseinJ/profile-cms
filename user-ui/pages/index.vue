56848bf7639978fffb778ec9d2ada64b56897227
Husein Jusic <husein.jusic@icloud.com>
2023-08-23 23:06:08 +0200

    implement get home query on index

--- user-ui/pages/index.vue
+++ user-ui/pages/index.vue
@@ -1,7 +1,16 @@
 <template>
-  <h1>fake index</h1>
+  <h1>Index of Page :)</h1>
+  <p>something is happening here.. soon (tm)</p>
 </template>
 
-<script>
-
+<script lang="ts" setup>
+const query = gql`
+  query Home{
+    home {
+      id
+      name
+    }
+  }
+`
+const { data } = await useAsyncQuery(query)
 </script>
