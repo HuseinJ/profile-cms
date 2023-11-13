3b71d1dcf4faa24440fc6ad34172e040c1a81358
Husein Jusic <husein.jusic@icloud.com>
2023-11-13 02:43:51 +0100

    feat: add 404 component

---
+++ user-ui/components/molecule/E404.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+import Paragraph from "~/components/atoms/Paragraph.vue";
+import Header from "~/components/atoms/Header.vue";
+</script>
+
+<template>
+  <title>404 Page not found :(</title>
+  <Header text="Whoops! 404 - The page could not be found" />
+  <iframe allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen frameborder="0"
+          height="200" src="https://www.youtube.com/embed/SiL2AjOtjZI?si=YAMJwmJX0xup5lCL"
+          title="YouTube video player"
+          width="300"></iframe>
+  <Paragraph text="Maybe try turning it off and on again?"  />
+</template>
--- user-ui/pages/[page].vue
+++ user-ui/pages/[page].vue
@@ -1,10 +1,17 @@
 
 <template>
-  <h1>{{data.page.name}}</h1>
-  <PageComponent v-for="component in data.page.pageComponents" :pageComponent="component"> test </PageComponent>
+  <div v-if="data.page">
+    <h1>{{data.page.name}}</h1>
+    <PageComponent v-for="component in data.page.pageComponents" :pageComponent="component"> test </PageComponent>
+  </div>
+  <E404 v-else/>
+
 </template>
 
 <script lang="ts" setup>
+
+import E404 from "~/components/molecule/E404.vue";
+
 const query = gql`
   query getPage($name: String!){
     page(name: $name) {
@@ -23,6 +30,4 @@ const query = gql`
 const route = useRoute();
 const variables = { name: route.params.page }
 const { data } = await useAsyncQuery(query, variables)
-
-console.log(data.value.page.pageComponents)
 </script>
